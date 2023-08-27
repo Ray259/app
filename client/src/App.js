19aba0ed5cf51c4ecd9fe2ebf76c2c69d247e5f0
Ray <elninothepea@gmail.com>
2023-08-27 23:03:54 +0700

    dev

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import { publicRoutes } from './routes';
+import { DefaultLayout } from './components/Layout';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Layout = route.layout || DefaultLayout;
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
